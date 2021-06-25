f8634b30fce585507e3d05a063156ba61f409b47
Gyuri Kim <artgr2062@konkuk.ac.kr>
2021-06-25 14:11:04 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0,
+
+  }
+  render() {
+    const { counter } = this.state;
+
+    return <div>{counter}</div>;
+  }
+  add = () => {
+    this.setState(prev => {
+      return {
+        coutner: prev.counter++;
+      }
+    })
+  }
 }
 
 export default App;
